125ba9bbff2bac68708735182316f992473d7ce0
Emre <emredirek1905@gmail.com>
2024-11-02 16:32:13 +0300

    UI

---
+++ Presentation/Vallet.UI/Controllers/Admin/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vallet.UI.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Presentation/Vallet.UI/Controllers/User/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vallet.UI.Controllers.User
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
